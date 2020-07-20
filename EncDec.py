2e8ad476a75053bebcd04de1ad513efd7a013abf
René de Hesselle <dehesselle@web.de>
2020-07-20 22:54:13 +0200

    Simplify code

--- EncDec.py
+++ EncDec.py
@@ -1,41 +1,34 @@
 # inspired by
 # https://nitratine.net/blog/post/encryption-and-decryption-in-python
 
-import base64
-import os
+from base64 import b64decode, b64encode
+from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.fernet import Fernet
+from os import urandom
 
 
 class EncDec:
-    def __init__(self):
-        self.salt = os.urandom(16)
-
-    def _generate_key(self, _password: str):
-        _password = _password.encode()  # convert to type bytes
+    @staticmethod
+    def generate_key(password: str, salt: bytes):
+        password = password.encode()
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
-            salt=self.salt,
+            salt=salt,
             iterations=100000,
             backend=default_backend()
         )
-        return base64.b64encode(kdf.derive(_password))
+        return b64encode(kdf.derive(password))
 
-    def _set_salt_from_crypttext(self, crypttext: str):
-        self.salt = base64.b64decode((crypttext[0:22] + "==").encode())
+    def encrypt(self, cleartext: str, password: str) -> str:
+        salt = urandom(16)
+        fernet = Fernet(self.generate_key(password, salt))
+        return (b64encode(salt)[0:22] + b64encode(fernet.encrypt(cleartext.encode()))).decode("UTF-8")
 
-    def _add_salt_to_crypttext(self, crypttext: str) -> str:
-        return base64.b64encode(self.salt).decode("UTF-8")[0:22] + crypttext
-
-    def encrypt(self, cleartext: str, _password: str) -> str:
-        fernet = Fernet(self._generate_key(_password))
-        return self._add_salt_to_crypttext(fernet.encrypt(cleartext.encode()).decode("UTF-8"))
-
-    def decrypt(self, crypttext: str, _password: str) -> str:
-        self._set_salt_from_crypttext(crypttext)
-        fernet = Fernet(self._generate_key(_password))
+    def decrypt(self, crypttext: str, password: str) -> str:
+        salt = b64decode((crypttext[0:22] + "==").encode())
         crypttext = crypttext[22:]  # remove salt
-        return fernet.decrypt(crypttext.encode()).decode("UTF-8")
+        fernet = Fernet(self.generate_key(password, salt))
+        return fernet.decrypt(b64decode(crypttext.encode())).decode("UTF-8")
