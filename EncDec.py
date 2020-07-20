e336db1beecfb1ce764d0958b91f15b869a4a1cb
René de Hesselle <dehesselle@web.de>
2020-07-20 23:01:52 +0200

    Remove class

--- EncDec.py
+++ EncDec.py
@@ -9,26 +9,26 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from os import urandom
 
 
-class EncDec:
-    @staticmethod
-    def generate_key(password: str, salt: bytes):
-        password = password.encode()
-        kdf = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=salt,
-            iterations=100000,
-            backend=default_backend()
-        )
-        return b64encode(kdf.derive(password))
+def generate_key(password: str, salt: bytes):
+    password = password.encode()
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return b64encode(kdf.derive(password))
 
-    def encrypt(self, cleartext: str, password: str) -> str:
-        salt = urandom(16)
-        fernet = Fernet(self.generate_key(password, salt))
-        return (b64encode(salt)[0:22] + b64encode(fernet.encrypt(cleartext.encode()))).decode("UTF-8")
 
-    def decrypt(self, crypttext: str, password: str) -> str:
-        salt = b64decode((crypttext[0:22] + "==").encode())
-        crypttext = crypttext[22:]  # remove salt
-        fernet = Fernet(self.generate_key(password, salt))
-        return fernet.decrypt(b64decode(crypttext.encode())).decode("UTF-8")
+def encrypt(cleartext: str, password: str) -> str:
+    salt = urandom(16)
+    fernet = Fernet(generate_key(password, salt))
+    return (b64encode(salt)[0:22] + b64encode(fernet.encrypt(cleartext.encode()))).decode("UTF-8")
+
+
+def decrypt(crypttext: str, password: str) -> str:
+    salt = b64decode((crypttext[0:22] + "==").encode())
+    crypttext = crypttext[22:]  # remove salt
+    fernet = Fernet(generate_key(password, salt))
+    return fernet.decrypt(b64decode(crypttext.encode())).decode("UTF-8")
